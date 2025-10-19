d09359be22f5cd68d30099e1c6831af68cf469d5
eduardoacunacoronado <edu191191@gmail.com>
2025-10-19 02:17:09 +0200

    Initial commit.

---
+++ src/test/java/com/ejemplo/interceptor/InterceptorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ejemplo.interceptor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InterceptorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
